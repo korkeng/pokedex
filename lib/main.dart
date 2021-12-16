88def5d6ff817320a467dbcbd9d46d6ec43c15f0
korkeng <chawanan.akk@mail.kmutt.ac.th>
2021-12-17 00:00:42 +0700

    Remove app title

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pokedex Application',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const PokemonScreen(),
     );
   }
