5cbd94f484f207d83297172fd33989e7e5875124
korkeng <chawanan.akk@mail.kmutt.ac.th>
2021-12-16 23:59:58 +0700

    Change widget returner

--- lib/pokemon/presentation/pokemon_screen.dart
+++ lib/pokemon/presentation/pokemon_screen.dart
@@ -1,10 +1,8 @@
+import 'package:app/common/constants/text_constants.dart';
 import 'package:flutter/material.dart';
 
 class PokemonScreen extends StatefulWidget {
-  const PokemonScreen({Key? key})
-      : super(
-          key: key,
-        );
+  const PokemonScreen({Key? key}) : super(key: key);
 
   @override
   _PokemonScreenState createState() => _PokemonScreenState();
@@ -13,8 +11,13 @@ class PokemonScreen extends StatefulWidget {
 class _PokemonScreenState extends State<PokemonScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(TextConstant.titlePage),
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
     );
   }
 }
